9861254008be5f410d45169c7c0d8f9ca058c924
shuygena <i.am.zuzya@gmail.com>
2022-09-11 20:05:04 +0300

    training_model: add DataFrame

--- training_model.py
+++ training_model.py
@@ -2,5 +2,5 @@ import pandas as pd
 import numpy as np 
 
 if __name__ == '__main__':
-    df = pd.read_csv('dat.csv', delimiter=',')
-    df.head()
+    df = pd.read_csv('data.csv', delimiter=',')
+    #print(df.head())
