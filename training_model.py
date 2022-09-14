ec91dd40c7728403eabab8c934c9552d05b5a7c4
Zukhra Khubieva <75737596+shuygena@users.noreply.github.com>
2022-09-14 23:37:12 +0300

    add plots, min_max_scale; fix gradient_descent
    gradient_descent() : df["km"] -> df["price"]

--- training_model.py
+++ training_model.py
@@ -1,26 +1,57 @@
 import pandas as pd 
-import numpy as np 
+import numpy as np
+import matplotlib.pyplot as plt
 
 lr = 0.01 # learning rate
 
-
-def estimate_price(theta, x): #predict (w[0] + w[1] * x)
+def plot_data_graph(df):
+    plt.scatter(df["km"], df["price"], color='b') 
+    #plt.plot(X_test, y_pred,color='k')
+    plt.title("Dataset") # заголовок
+    plt.xlabel("mileage") # ось абсцисс
+    plt.ylabel("price") # ось ординат
+    plt.show()
+    
+def plot_line(df, y_pred):
+    plt.scatter(df["km"], df["price"], color='b')
+    plt.plot(df["km"], y_pred, color="r")
+    plt.title("Regression line")
+    plt.xlabel("mileage") 
+    plt.ylabel("price")
+    #plt.savefig('result.png')
+    plt.show()
+    
+def estimate_price(theta, x): # predict (w[0] + w[1] * x)
     return theta[0] + theta[1] * x
 
-def gradient_descent(y_pred, df, x, tmp_theta):
-    tmp_theta[0] -= lr / df["km"].shape[0] * np.sum(y_pred - df["km"]) 
-    tmp_theta[1] -= lr / df["km"].shape[0] * np.sum((y_pred - df["km"]) * x)
+def gradient_descent(y_pred, df, x, tmp_theta): 
+    tmp_theta[0] -= lr / df.shape[0] * np.sum(y_pred - df["price"]) 
+    tmp_theta[1] -= lr / df.shape[0] * np.sum((y_pred - df["price"]) * x)
     return tmp_theta
 
-def train(df, epochs = 5000):
+def min_max_scaler(x, dataset): # предобработка данных
+    x_min = dataset.min()
+    x_max = dataset.max()
+    return (x - x_min) / (x_max - x_min)
+
+def fit(df, epochs = 5000): # обучение
     tmp_theta = [ 0 for i in range(2)]
-    x = 0
+    x = min_max_scaler(df["km"], df["km"])
+    #print("x:", x)
+    #x = df["km"]
     for i in range(epochs):
         y_pred = estimate_price(tmp_theta, x) # 0 - вычислить что подставить
-        tmp_theta = gradient_descent(y_pred, df, x,  tmp_theta0, tmp_theta1)
+        tmp_theta = gradient_descent(y_pred, df, x,  tmp_theta)
+    plot_line(df, y_pred)
+    return tmp_theta
 
 
 if __name__ == '__main__':
     df = pd.read_csv('data.csv', delimiter=',')
+    plot_data_graph(df)
+    #theta = fit(df, 5000)
+    #print("price =", estimate_price(theta, min_max_scaler(50000, df["km"]))) # пробуем предсказать цену
+    #print("theta[0] =", theta[0], "theta[1] =", theta[1])
+    
     #print(df.head())
-    #print(df["km"])
+    # print(df["km"])
