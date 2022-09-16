881cc986f3e97e220fcb3a63e96239d21c27d9e1
shuygena <i.am.zuzya@gmail.com>
2022-09-16 20:13:04 +0300

    clean predict.py

--- predict.py
+++ predict.py
@@ -1,16 +1,8 @@
 import pandas as pd
-# import min_max_scaler from training_model
-# import estimate_price from training_model
 from training_model import min_max_scaler
 from training_model import estimate_price
 
 def predict():
-    # mileage = float(input("Please, enter mileage: "))
-    # theta = pd.read_csv('theta.csv', delimiter=',')
-    # theta = theta["theta"].tolist()
-    # df = pd.read_csv('data.csv', delimiter=',')      
-    # x = min_max_scaler(mileage, df["km"])
-    # price = estimate_price(theta, x)
 
     try:
         mileage = float(input("\033[34mPlease, enter mileage: \033[0m"))
@@ -22,7 +14,7 @@ def predict():
             theta = theta["theta"].tolist()
             x = min_max_scaler(mileage, df["km"])
             price = estimate_price(theta, x)
-            if price > 0:
+            if price >= 0:
                 print("Price = ", int(price))
             else:
                 print("Price is too low")
